64feef6412f6efd3f44408c3b1076ef6c8008b94
Broda Noel <brodanoel@gmail.com>
2018-02-27 12:28:16 -0300

    Implement ColorPicker when widget is `color`.

--- src/components/widgets/ColorWidget.js
+++ src/components/widgets/ColorWidget.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function ColorWidget(props) {
-  const { disabled, readonly, registry: { widgets: { BaseInput } } } = props;
-  return <BaseInput type="color" {...props} disabled={disabled || readonly} />;
-}
+import { ColorPicker } from '@carecloud/material-cuil';
 
-if (process.env.NODE_ENV !== 'production') {
-  ColorWidget.propTypes = {
-    schema: PropTypes.object.isRequired,
-    id: PropTypes.string.isRequired,
-    value: PropTypes.string,
-    required: PropTypes.bool,
-    disabled: PropTypes.bool,
-    readonly: PropTypes.bool,
-    autofocus: PropTypes.bool,
-    onChange: PropTypes.func,
-  };
-}
+const ColorWidget = props => {
+  const {
+    value,
+    /**
+     * @description By default, `react-jsonschema-form` is going to disable
+     * the alpha value in the picker
+     * @default `true`
+     * @type {Boolean}
+     */
+    disableAlpha = true,
+    ...otherProps
+  } = props;
+
+  return <ColorPicker color={value} disableAlpha={disableAlpha} {...otherProps} />;
+};
+
+ColorWidget.propTypes = {
+  /**
+   * @description The default value can be set using this prop
+   * @type {String}
+   */
+  value: PropTypes.string,
+};
 
 export default ColorWidget;
