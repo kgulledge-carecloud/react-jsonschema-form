1a1b7987e69f70ce92d4c127e049c617ac6c4f91
Juan Norris <jnorris@carecloud.com>
2018-03-20 14:00:29 -0300

    Bump @carecloud/material-cuil@0.12.5. Fix CheckboxWidget value.

--- src/components/widgets/CheckboxWidget.js
+++ src/components/widgets/CheckboxWidget.js
@@ -5,6 +5,7 @@ import { FormControlLabel, Checkbox } from '@carecloud/material-cuil';
 
 function CheckboxWidget(props) {
   const { schema, id, value, required, disabled, readonly, label, onChange } = props;
+
   return (
     <div className={`checkbox ${disabled || readonly ? 'disabled' : ''}`}>
       {schema.description && <DescriptionField description={schema.description} />}
@@ -14,7 +15,7 @@ function CheckboxWidget(props) {
           <Checkbox
             id={id}
             checked={typeof value === 'undefined' ? false : value}
-            value={value}
+            value={value ? value.toString() : ''}
             required={required}
             disabled={disabled || readonly}
             onChange={event => onChange(event.target.checked)}
@@ -34,7 +35,7 @@ if (process.env.NODE_ENV !== 'production') {
   CheckboxWidget.propTypes = {
     schema: PropTypes.object.isRequired,
     id: PropTypes.string.isRequired,
-    value: PropTypes.bool,
+    value: PropTypes.oneOf([PropTypes.bool, PropTypes.string]),
     required: PropTypes.bool,
     disabled: PropTypes.bool,
     readonly: PropTypes.bool,
